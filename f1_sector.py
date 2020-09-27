736f807cbd63fa300a91215e94c595cb56e5e267
RobertZetterlund <robert.zetterlund@outlook.com>
2020-09-27 15:47:49 +0200

    add circuits

--- f1_sector.py
+++ f1_sector.py
@@ -1,56 +1,64 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import re
-import time
-from datetime import datetime
-import matplotlib.dates as mdates
-import matplotlib.ticker as ticker
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import requests
+import argparse
+import os
 
+# Setup argument parser
 # since site does not allow webscraping (403) just copy html to file
 # http://en.mclarenf-1.com/index.php?page=results&gp=1047&s=7992&p=3
+parser = argparse.ArgumentParser(
+    description="creates a dataframe given a path to html of a circuit from mclarenf-1.com (http://en.mclarenf-1.com/index.php?page=results&gp=1047&s=7992&p=3)"
+)
+parser.add_argument("--circuit", type=str,
+                    help="folder name of circuit", default="mugello")
 
-html_file = open('page.html', 'r')
-html = html_file.read()
+args = parser.parse_args()
 
-soup = BeautifulSoup(html, 'html.parser')
-results = soup.find(id="results")
-tbody = results.find("tbody", {"role": "alert"})
-content = tbody.findAll("tr")
-
+# setup df
 cols = ["Position", "Driver no.", "Driver",
         "First Sector", "Second Sector", "Third Sector", "Lap Time"]
-
 df = pd.DataFrame(columns=cols)
 
-# only interested in lap time
+### Setup path
+circuit = args.circuit
+path = os.path.join("circuits/" + circuit + ".html")
+
+html_file = open(path, 'r')
+html = html_file.read()
+# get soup
+soup = BeautifulSoup(html, 'html.parser')
+# get sum of results
+sumResults = soup.find({"div"}, id="tabs-0")
+# find table with results
+results = sumResults.find({"table"}, id="results")
+# get table
+table = results.find("tbody", {"role": "alert"})
+# get all rows
+tablerows = table.findAll("tr")
+
+# times are labeled with position "time [x]"", remove brackets and only get time.
 def removeBrackets(s_times):
     return(list(map(lambda time: time.split(" ", 1)[0], s_times)))
 
+# Gets relevant data from row
 def getSectorTimes(row):
-    first = row.findAll("td")
-    mylist = list(map(lambda td: td.text, first))
-    position, number, name, s_one, s_two, s_three, total = mylist[:7]
+    tableData = row.findAll("td")
+    tableDataList = list(map(lambda td: td.text, tableData))
+    position, number, name, s_one, s_two, s_three, total = tableDataList[:7]
     s_one, s_two, s_three = removeBrackets([s_one, s_two, s_three])
 
     dictionary = {"Position": position, "Driver no.": number, "Driver": name,
                   "First Sector": s_one, "Second Sector": s_two, "Third Sector": s_three, "Lap Time": total}
 
-    df_row = pd.DataFrame(data=dictionary, index=[0])
-    
-    return df_row
+    return pd.DataFrame(data=dictionary, index=[0])
 
 
-def getDataFrame(content):
-    return list(map(lambda tr: getSectorTimes(tr), content))
+def getDataFrames(tablerows):
+    return list(map(lambda row: getSectorTimes(row), tablerows))
 
-df_list = getDataFrame(content)
+listOfDataFrames = getDataFrames(tablerows)
 
-df = pd.concat(df_list).reset_index()
+df = pd.concat(listOfDataFrames).reset_index(drop=True)
 
 print(df)
-
